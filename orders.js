46249b94f90721a3246a36f43a92acef69550615
Senad Zenkovic <zenkovic88@gmail.com>
2021-11-19 11:16:19 -0700

    complete orders exercise

--- orders.js
+++ orders.js
@@ -29,7 +29,18 @@
     stored on the object.
 */
 
-//CODE HERE
+class Ticket {
+    constructor(items, orderTime, customerId){
+        this.items = items
+        this.orderTime = orderTime
+        this.customerId = customerId
+        this.status = "queued"
+    }
+    updateStatus(newStatus){
+        this.status = newStatus
+        console.log(`The order for customer ${this.customerId} is now ${this.status}!`)
+    }
+}
 
 
 
@@ -44,7 +55,7 @@
     customer: 575
 */
 
-//CODE HERE
+const firstTicket = new Ticket("Pizza, Bread, Soda", "7:03 PM", 575 )
 
 
 /*
@@ -53,4 +64,5 @@
     'cooking'
 */
 
-//CODE HERE
+firstTicket.updateStatus("cooking");
+console.log(firstTicket);
